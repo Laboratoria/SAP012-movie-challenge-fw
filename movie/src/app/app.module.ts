10ffd250df3cc8dfdb7b3564286be41c4c323810
Elizabete <elizabete958@gmail.com>
2024-03-22 21:15:00 -0300

    Add cod dos components

--- movie/src/app/app.module.ts
+++ movie/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieCardComponent } from './layout/movie-card/movie-card.component';
+import { APIService } from './shared/services/api.service';
+import { HeaderComponent } from './layout/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieCardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [
+    APIService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
