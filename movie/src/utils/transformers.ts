bfbf5877f4737b1f39594e2db337474c5f996b84
Elizabete <elizabete958@gmail.com>
2024-03-22 21:09:56 -0300

    Add baseUrl de imagens e suas configurações

--- movie/src/utils/transformers.ts
+++ movie/src/utils/transformers.ts
@@ -1,11 +1,12 @@
 import { Movie } from "src/models/Movie";
 
 export function formatMovie(apiMovieData: any): Movie{
+  const baseUrl = 'https://image.tmdb.org/t/p/w500'; // URL base da API TMDB para imagens
 
   const formattedMovie: Movie = {
     id: apiMovieData.id,
-    title: apiMovieData.title || "Unknown Title",
-    image_path: apiMovieData.poster_path,
+    title: apiMovieData.title,
+    image_path: `${baseUrl}${apiMovieData.poster_path}`,
     release_year: new Date(apiMovieData.release_date).getFullYear().toString(),
   };
 
