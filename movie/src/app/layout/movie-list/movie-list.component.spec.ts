0f475eece579a76c1be64a4b4bb20c4e72bd492c
Elizabete <elizabete958@gmail.com>
2024-03-24 19:40:15 -0300

    Add teste de movie list

--- movie/src/app/layout/movie-list/movie-list.component.spec.ts
+++ movie/src/app/layout/movie-list/movie-list.component.spec.ts
@@ -3,6 +3,7 @@ import { MovieListComponent } from './movie-list.component';
 import { Movie } from 'src/models/Movie';
 import { MovieCardComponent } from '../movie-card/movie-card.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
 
 describe('MovieListComponent', () => {
   let component: MovieListComponent;
@@ -11,7 +12,7 @@ describe('MovieListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MovieListComponent, MovieCardComponent],
-      imports: [HttpClientModule],
+      imports: [HttpClientModule, MatCardModule],
     }).compileComponents();
   });
 
@@ -24,53 +25,46 @@ describe('MovieListComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render movie list', () => {
+  it('deve renderizar a lista de filmes', () => {
     const movies: Movie[] = [
-      {
-        id: 1,
-        title: 'Movie 1',
-        image_path: 'path/to/image1.jpg',
-        release_year: '2022',
-      },
-      {
-        id: 2,
-        title: 'Movie 2',
-        image_path: 'path/to/image2.jpg',
-        release_year: '2023',
-      },
+      { id: 1, title: 'Movie 1', image_path: 'path/to/image1.jpg', release_year: '2022' },
+      { id: 2, title: 'Movie 2', image_path: 'path/to/image2.jpg', release_year: '2023' }
     ];
 
-    console.log('Movies:', movies); // Adiciona este console.log para verificar os dados dos filmes
+    console.log('Movies:', movies);
 
     component.movies = movies;
     fixture.detectChanges();
 
     const movieCardElements =
       fixture.nativeElement.querySelectorAll('app-movie-card');
-    console.log('Movie card elements:', movieCardElements); // Adiciona este console.log para verificar os elementos movie-card
+    console.log('Elementos do Cards de filme:', movieCardElements);
 
     expect(movieCardElements.length).toBe(movies.length);
 
     movieCardElements.forEach(
       (movieCardElement: HTMLElement, index: number) => {
         const movie = movies[index];
-        expect(movieCardElement.getAttribute('title')).toContain(movie.title);
-        expect(movieCardElement.getAttribute('release_year')).toContain(
-          movie.release_year
-        );
-        expect(movieCardElement.getAttribute('poster_path')).toContain(
-          movie.image_path
-        );
+        console.log('Movie:', movie);
+        console.log('Title:', movie.title);
+        console.log('Release Year:', movie.release_year);
+        console.log('Image Path:', movie.image_path);
+        expect(movieCardElement.textContent).toContain(movie.title);
+        expect(movieCardElement.textContent).toContain(movie.release_year);
+        // Certifique-se de que o atributo 'src' contém o caminho da imagem
+        const imgElement = movieCardElement.querySelector('img');
+        expect(imgElement).toBeTruthy(); // Verifica se existe um elemento de imagem dentro do movieCardElement
+        if (imgElement) {
+            expect(imgElement.getAttribute('src')).toContain(movie.image_path);
+        }
       }
     );
   });
 
-  it('should render "No movies found" message for empty movie list', () => {
-    // Deixando a lista de filmes vazia
+  it('deve renderizar a mensagem "No movies found" para lista de filmes vazia', () => {
     component.movies = [];
     fixture.detectChanges();
 
-    // Verificando se a mensagem "No movies found" está presente
     const messageElement =
       fixture.nativeElement.querySelector('.no-movies-message');
     expect(messageElement).toBeTruthy(); // Corrigindo o erro aqui
