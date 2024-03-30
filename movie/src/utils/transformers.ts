ee798dfe8b4fd68ef0cb2266afa0c4dccc62e054
Elizabete <elizabete958@gmail.com>
2024-03-29 21:35:01 -0300

    Add objeto: genres e deixando ele pronto para mapear os generos para um array de strings

--- movie/src/utils/transformers.ts
+++ movie/src/utils/transformers.ts
@@ -8,6 +8,7 @@ export function formatMovie(apiMovieData: any): Movie{
     title: apiMovieData.title,
     image_path: apiMovieData.poster_path ? `${baseUrl}${apiMovieData.poster_path}` : '/image.png',
     release_year: apiMovieData.release_date ? new Date(apiMovieData.release_date).getFullYear().toString() : 'N/A',
+    genres: apiMovieData.genres_ids ? apiMovieData.genres_ids.map((genre: { name: string }) => genre.name) : []
   };
 
   return formattedMovie;
