c83e5aa10b824efa31dfa4cfdc0c9b38f3d9867e
Elizabete <elizabete958@gmail.com>
2024-03-30 01:19:06 -0300

    Desenvolvimento da função formatGenresToOptions

--- movie/src/utils/transformers.ts
+++ movie/src/utils/transformers.ts
@@ -1,25 +1,19 @@
 import { Movie } from 'src/models/Movie';
 
-export function formatMovie(apiMovieData: any): Movie {
+export function formatMovie(apiMovieData: any, genresMap: Map<number, string>): Movie {
   const baseUrl = 'https://image.tmdb.org/t/p/w500';
-
-  const formattedMovie: Movie = {
-    id: apiMovieData.id || 1011985,
-    title: apiMovieData.title || 'Kung Fu Panda 4',
+  const genres: string[] = apiMovieData.genre_ids
+    ? apiMovieData.genre_ids.map((genreId: number) => genresMap.get(genreId) || 'Gênero não encontrado!')
+    : ['Gênero não disponível'];
+  return {
+    id: apiMovieData.id,
+    title: apiMovieData.title,
     image_path: apiMovieData.poster_path ? `${baseUrl}${apiMovieData.poster_path}` : 'https://image.tmdb.org/t/p/w500/wkfG7DaExmcVsGLR4kLouMwxeT5.jpg',
-    release_year: apiMovieData.release_date
-      ? new Date(apiMovieData.release_date).getFullYear().toString()
-      : 'N/A',
-    genres: apiMovieData.genres_ids || ['Action', 'Adventure', 'Animation', 'Comedy', 'Family'],
+    release_year: apiMovieData.release_date ? new Date(apiMovieData.release_date).getFullYear().toString() : 'Desconhecido',
+    genres
   };
-
-  return formattedMovie;
 }
 
-export function formatGenresToMap(genreData: Array<{ id: number; name: string }>): Map<number, string> {
-  const genresMap = new Map<number, string>();
-  genreData.forEach(genre => {
-      genresMap.set(genre.id, genre.name);
-  });
-  return genresMap;
+export function formatGenresToMap(genresData: { id: number, name: string }[]): Map<number, string> {
+  return new Map(genresData.map(genre => [genre.id, genre.name]));
 }
