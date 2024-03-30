6d266604af25b6ea0aa9e2a3271bdc095246c733
Elizabete <elizabete958@gmail.com>
2024-03-30 09:56:28 -0300

    Add test e select e suas funçoes

--- movie/src/app/layout/list-options/list-options.component.spec.ts
+++ movie/src/app/layout/list-options/list-options.component.spec.ts
@@ -1,21 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ListOptionsComponent } from './list-options.component';
+import { FormsModule } from '@angular/forms';
 
 describe('ListOptionsComponent', () => {
   let component: ListOptionsComponent;
   let fixture: ComponentFixture<ListOptionsComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ListOptionsComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [ ListOptionsComponent ]
+    })
+    .compileComponents();
+
     fixture = TestBed.createComponent(ListOptionsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('deve criar', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve emitir onChange quando uma opção é selecionada', () => {
+    spyOn(component.onChange, 'emit');
+    const mockOption = { value: 'testValue', label: 'Test Label' };
+    component.selectOption(mockOption);
+    expect(component.onChange.emit).toHaveBeenCalledWith(mockOption);
+  });
+
+  it('deve emitir onClear quando a seleção for desmarcada', () => {
+    spyOn(component.onClear, 'emit');
+    component.clearSelection();
+    expect(component.onClear.emit).toHaveBeenCalled();
   });
 });
--- movie/src/app/layout/list-options/list-options.component.ts
+++ movie/src/app/layout/list-options/list-options.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-list-options',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-options.component.css']
 })
 export class ListOptionsComponent {
+  @Input() options: { value: string; label: string }[] = [];
+  @Input() selectedOption: { value: string; label: string } | null = null;
 
+  @Output() onChange: EventEmitter<{ value: string; label: string }> = new EventEmitter();
+  @Output() onClear: EventEmitter<void> = new EventEmitter();
+
+  constructor() { }
+
+  selectOption(option: { value: string; label: string }): void {
+    this.selectedOption = option;
+    this.onChange.emit(this.selectedOption);
+  }
+
+  clearSelection(): void {
+    this.selectedOption = null;
+    this.onClear.emit();
+  }
 }
