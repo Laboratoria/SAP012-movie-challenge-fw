01cc3cf9ab641ba612a9c2168e7302054f972256
Elizabete <elizabete958@gmail.com>
2024-03-29 22:31:45 -0300

    Adicionando o objeto genres

--- movie/src/utils/transformers.ts
+++ movie/src/utils/transformers.ts
@@ -1,14 +1,18 @@
-import { Movie } from "src/models/Movie";
+import { Movie } from 'src/models/Movie';
 
-export function formatMovie(apiMovieData: any): Movie{
+export function formatMovie(apiMovieData: any): Movie {
   const baseUrl = 'https://image.tmdb.org/t/p/w500';
 
   const formattedMovie: Movie = {
     id: apiMovieData.id,
     title: apiMovieData.title,
-    image_path: apiMovieData.poster_path ? `${baseUrl}${apiMovieData.poster_path}` : '/image.png',
-    release_year: apiMovieData.release_date ? new Date(apiMovieData.release_date).getFullYear().toString() : 'N/A',
-    genres: apiMovieData.genres_ids ? apiMovieData.genres_ids.map((genre: { name: string }) => genre.name) : []
+    image_path: apiMovieData.poster_path
+      ? `${baseUrl}${apiMovieData.poster_path}`
+      : '/image.png',
+    release_year: apiMovieData.release_date
+      ? new Date(apiMovieData.release_date).getFullYear().toString()
+      : 'N/A',
+    genres: apiMovieData.genres_ids || ['Gênero não disponível'],
   };
 
   return formattedMovie;
