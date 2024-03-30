63d09132fb2d6fc29973679bb61bf80c1fc0acf6
Elizabete <elizabete958@gmail.com>
2024-03-30 01:17:15 -0300

    Garantindo que api com o retorno correto do array de objetos

--- movie/src/app/shared/services/api.service.ts
+++ movie/src/app/shared/services/api.service.ts
@@ -1,55 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { environment } from './../../../environments/environment';
 import { Movie } from 'src/models/Movie';
-import { formatMovie } from 'src/utils/transformers';
+import { formatMovie, formatGenresToMap } from 'src/utils/transformers';
 
 @Injectable({
   providedIn: 'root',
 })
 export class APIService {
   constructor(private http: HttpClient) {}
-  getMovies(
-    filters: { page: number } = { page: 1 }
-  ): Observable<{
-    filters: { page: number };
-    metaData: { pagination: { currentPage: number; totalPages: number } };
-    movies: Movie[];
-  }> {
-    const page = filters.page || 1;
-    const url = `https://api.themoviedb.org/3/discover/movie?page=${page}`;
-    const headers = {
-      Authorization: `Bearer ${environment.TOKEN_API}`,
-    };
 
-    return this.http.get<any>(url, { headers }).pipe(
-      map((apiResponse: any) => ({
-        filters: { page: apiResponse.page },
-        metaData: {
-          pagination: {
-            currentPage: apiResponse.page,
-            totalPages: apiResponse.total_pages,
-          },
-        },
-        movies: apiResponse.results.map((movie: any) => formatMovie(movie)),
-      }))
+  getMovies(filters: { page: number } = { page: 1 }): Observable<{ filters: { page: number }, metaData: { pagination: { currentPage: number; totalPages: number } }, movies: Movie[] }> {
+    return this.getMovieGenres().pipe(
+      switchMap(genresArray => {
+        const genresMap = formatGenresToMap(genresArray);
+
+        const page = filters.page || 1;
+        const url = `https://api.themoviedb.org/3/discover/movie?page=${page}`;
+        const headers = {
+          'Authorization': `Bearer ${environment.TOKEN_API}`,
+        };
+
+        return this.http.get<any>(url, { headers }).pipe(
+          map(apiResponse => ({
+            filters: { page: apiResponse.page },
+            metaData: {
+              pagination: {
+                currentPage: apiResponse.page,
+                totalPages: apiResponse.total_pages,
+              },
+            },
+            movies: apiResponse.results.map((movie: any) => formatMovie(movie, genresMap)), // Agora passando genresMap
+          }))
+        );
+      })
     );
   }
 
   getMovieGenres(): Observable<{ id: number; name: string }[]> {
     const url = 'https://api.themoviedb.org/3/genre/movie/list';
     const headers = {
-      Authorization: `Bearer ${environment.TOKEN_API}`,
+      'Authorization': `Bearer ${environment.TOKEN_API}`,
     };
     return this.http.get<any>(url, { headers }).pipe(
-      map((apiResponse: any) =>
-        apiResponse.genres.map((genre: any) => ({
-          id: genre.id,
-          name: genre.name,
-        }))
-      )
+      map(apiResponse => apiResponse.genres)
     );
   }
 }
