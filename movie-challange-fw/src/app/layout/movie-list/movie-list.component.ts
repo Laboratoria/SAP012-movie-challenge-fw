993a705b6c76d7dadc343c183d040b6b480a34f1
sellylima <vanessa.limanasc@gmail.com>
2024-06-18 21:32:04 -0300

    ajustes movie-list

--- movie-challange-fw/src/app/layout/movie-list/movie-list.component.ts
+++ movie-challange-fw/src/app/layout/movie-list/movie-list.component.ts
@@ -1,17 +1,17 @@
 // Componente que exibe uma lista de filmes
-import { Component, Input } from '@angular/core';
-import { Movie } from 'src/models/Movie';
+// import { Component, Input } from '@angular/core';
+// import { Movie } from 'src/models/Movie';
 
 
-@Component({
-  selector: 'app-movie-list',
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
-})
+// @Component({
+//   selector: 'app-movie-list',
+//   templateUrl: './movie-list.component.html',
+//   styleUrls: ['./movie-list.component.css']
+// })
 
-// **MovieListComponent** é responsável apenas por exibir uma lista de filmes e reutilizável que pode ser usado em diferentes partes da aplicação para exibir listas de filmes. ****@Input**** permite que **MovieListComponent** receba dados do componente pai, promovendo a comunicação e a reutilização do componente.
+// // **MovieListComponent** é responsável apenas por exibir uma lista de filmes e reutilizável que pode ser usado em diferentes partes da aplicação para exibir listas de filmes. ****@Input**** permite que **MovieListComponent** receba dados do componente pai, promovendo a comunicação e a reutilização do componente.
 
-export class MovieListComponent {
-  @Input() movies: Movie[] = [];  //Definir um Input chamado movies do tipo Movie[] que represente um array de modelos de negócios Movie ou seja // Array de filmes recebido do componente pai //
-}
+// export class MovieListComponent {
+//   @Input() movies: Movie[] = [];  //Definir um Input chamado movies do tipo Movie[] que represente um array de modelos de negócios Movie ou seja // Array de filmes recebido do componente pai //
+// }
 
